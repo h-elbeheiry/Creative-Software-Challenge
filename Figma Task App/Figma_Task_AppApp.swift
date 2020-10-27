612a3c0b95a2582aedda5cbc851e264d3af88421
Hussein Elbeheiry <hussein@elbeheiry.com>
2020-10-27 14:19:00 -0400

    Initial Commit

---
+++ Figma Task App/Figma_Task_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Figma_Task_AppApp.swift
+//  Figma Task App
+//
+//  Created by Hussein Elbeheiry on 10/27/20.
+//
+
+import SwiftUI
+
+@main
+struct Figma_Task_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
